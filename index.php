edd144bbe4fb057936287e71416f912792f985fc
Sandesh Tiwari <sandeshtiwari@live.com>
2018-02-18 10:46:20 -0600

    fix: Change the name to the input tags

--- index.php
+++ index.php
@@ -16,17 +16,17 @@
   </head>
   <body>
     <div class='container'>
-      <form>
+      <form action = 'login.php' method = 'POST'>
         <div class="form-group">
           <label for="exampleInputEmail1">Email address</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+          <input name='email' type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
           <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
         </div>
         <div class="form-group">
           <label for="exampleInputPassword1">Password</label>
-          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+          <input name='password' type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-primary" name='login_button'>Submit</button>
       </form>
     </div>
 

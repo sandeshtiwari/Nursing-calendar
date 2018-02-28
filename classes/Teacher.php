bf6c43ed02d0cb83b4d8584c9c2ffd28f768e5e6
Sandesh Tiwari <sandeshtiwari@live.com>
2018-02-28 11:22:29 -0600

    Fix: Initialize $rows to avoid error message

--- classes/Teacher.php
+++ classes/Teacher.php
@@ -12,6 +12,7 @@
 		{
 			$string = "SELECT Course_ID, Prefix, Number, Title FROM course, teacher WHERE course.Teacher_CWID = teacher.CWID AND teacher.email = '$this->email'";
 			$check_database_query = mysqli_query($this->con, $string);
+			$rows = array();
 			while($row = mysqli_fetch_assoc($check_database_query))
 			{
 				$rows[] = $row['Course_ID']. " ". $row['Prefix']." ". $row['Number']. " ". $row['Title'];

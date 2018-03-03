d0a2b62ecfecc216208f6f3385a037e3eaeb8955
Sandesh Tiwari <sandeshtiwari@live.com>
2018-03-03 11:25:48 -0600

    fix: Redirect a student trying to access bookRoom.php

--- bookRoom.php
+++ bookRoom.php
@@ -2,7 +2,7 @@
   require "config/config.php";
   error_reporting(0);
   // authenticating the teacher or the person logged in
-  if(isset($_GET['email']) && isset($_SESSION['email']))
+  if(isset($_GET['email']) && isset($_SESSION['email']) && $_SESSION['privilege'] != 'student')
   {
     if(!password_verify($_SESSION['email'], $_GET['email']))
     {

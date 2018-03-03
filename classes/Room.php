9e1a4c0225adc395d51b68e38dfb062f5f6f7c35
Sandesh Tiwari <sandeshtiwari@live.com>
2018-03-03 17:19:17 -0600

    feat: Add functions to get start and end time of a class

--- classes/Room.php
+++ classes/Room.php
@@ -10,11 +10,22 @@
 		}
 		public function showRooms($course_id)
 		{
-
+			$startTime = $this->getStartTime($course_id);
+			$endTime = $this->getEndTime($course_id);
 		}
 		private function getStartTime($course_id)
 		{
-
+			$string = "SELECT Start_time FROM course WHERE Course_id = '$course_id' LIMIT 1";
+			$query = mysqli_query($this->con, $string);
+			$row = mysqli_fetch_assoc($query);
+			return $row[0];
+		}
+		private function getEndTime($course_id)
+		{
+			$string = "SELECT End_time FROM course WHERE Course_id = '$course_id' LIMIT 1";
+			$query = mysqli_query($this->con, $string);
+			$row = mysqli_fetch_assoc($query);
+			return $row[0];
 		}
 	}
 ?>

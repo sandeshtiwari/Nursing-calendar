dc443454248c414226c80ca0ef37757f01f303f0
Sandesh Tiwari <sandeshtiwari@live.com>
2018-03-12 02:57:04 -0500

    bug: Fix a link in some pages

--- admin_page.php
+++ admin_page.php
@@ -29,7 +29,7 @@ if ($_SESSION['privilege'] != 'admin' || isset($_SESSION['email']))
 <body class="fixed-nav sticky-footer bg-dark" id="page-top">
   <!-- Navigation-->
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
-    <a class="navbar-brand" href="index.html">Nursing Admin</a>
+    <a class="navbar-brand" href="admin_page.php">Nursing Admin</a>
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>

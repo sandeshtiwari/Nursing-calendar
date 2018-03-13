7769bb322f08d53c2097ff8edca3a764d9005823
sanjogpokhrel <sanjogpokhrel@outlook.com>
2018-03-12 22:51:56 -0500

    Updated calendar.php remodled the desing, still few things need to be done

--- calendar.php
+++ calendar.php
@@ -1,34 +1,34 @@
 <?php
-	require 'check_privilege.php';
+  require 'check_privilege.php';
   require 'classes/Teacher.php';
   require 'classes/Student.php';
   require 'classes/Admin.php';
-	if(!isset($_SESSION['username']))
-	{
-		header("Location: index.php");
-	}
-	$_SESSION['privilege'] = 'admin';
-	if(check_student($con))
-	{
-		$_SESSION['privilege'] = 'student';
-	}
-	else if(check_teacher($con))
-	{
-		$_SESSION['privilege'] = 'teacher';
-	}
-	echo "Welcome ". $_SESSION['username']."<br/>";
-	echo $_SESSION['privilege'];
-	$display = "";
-	if($_SESSION['privilege'] == 'admin')
-	{
-		$display = "<a href='admin_page.php'>My Admin Page</a>";
+  if(!isset($_SESSION['username']))
+  {
+    header("Location: index.php");
+  }
+  $_SESSION['privilege'] = 'admin';
+  if(check_student($con))
+  {
+    $_SESSION['privilege'] = 'student';
+  }
+  else if(check_teacher($con))
+  {
+    $_SESSION['privilege'] = 'teacher';
+  }
+  echo "Welcome ". $_SESSION['username']."<br/>";
+  echo $_SESSION['privilege'];
+  $display = "";
+  if($_SESSION['privilege'] == 'admin')
+  {
+    $display = "<a href='admin_page.php'></a>";
     $person = new Admin($con, $_SESSION['email']);
-	}
-	else if($_SESSION['privilege'] == 'teacher')
-	{
-		$display = "<a href='register.php'>Register Classroom</a>";
+  }
+  else if($_SESSION['privilege'] == 'teacher')
+  {
+    $display = "<a href='register.php'>Register Classroom</a>";
     $person = new Teacher($con, $_SESSION['email']);
-	}
+  }
   else
   {
     $person = new Student($con, $_SESSION['email']);
@@ -52,9 +52,16 @@
 
 <head>
 
+
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.css" />
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.css" />
+
+<link rel="stylesheet" href="jquery-ui/jquery-ui.css" />
+
+<link rel="stylesheet" href="jquery-ui/jquery-ui.css" />
+
+<link rel="stylesheet" href="jquery-ui/jquery-ui.theme.css" />
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
@@ -87,6 +94,10 @@
 
         defaultView: 'month',
         nowIndicator: true,
+        theme: true,
+        default: 'false',
+
+
         header: {
         left: 'prev,next today',
         center: 'title',
@@ -120,16 +131,63 @@
 
   font-family: "Lucida Grande",Helvetica,Arial,Verdana,sans-serif;
 
+  background-color: #F5F5F5;
   }
 
  #calendar {
 
   width: 650px;
 
-  margin: 0 auto;
+  margin: 0 
+
+  auto;
 
   }
 
+  ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #AA1F01;
+}
+
+li {
+    float: left;
+    border-right:1px solid #bbb;
+}
+
+li:last-child {
+    border-right: none;
+}
+
+li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+
+li a:hover:not(.active) {
+    background-color: #EBAF0F;
+}
+
+.active {
+    background-color: #870505;
+}
+img {  
+  
+    background: url('ulm_logo.png');
+    background-repeat:repeat-x;
+    background-position:top;
+    margin-top:90px;
+}
+
+
+
+}
+ 
 </style>
 
 </head>
@@ -137,15 +195,39 @@
 
 <body>
 
- <h2>Nursing Calendar</h2>
- <a href="logout.php">Logout</a>
+
+
+<ul>
+  
+<a class="navbar-brand" rel="home" href="#" title="University of Louisiana at Monroe">
+        <img style="max-width:140px; margin-top: 7px;"
+             src="ulm_logo.png" class="img-circle" align="middle" >
+    </a>
+ 
+
+  
+
+  <li><a href="#home">Home</a></li>
+  <li><a href="#news">News</a></li>
+ 
+
+  <li style="float:right"><a href="logout.php">Logout</a></li>
+  <li style="float:right"><a href="admin_page.php">My Admin Page</a></li>
+</ul> 
+
+ <h2 style="color:#AA1F01";>Nursing Calendar</h2>
+ 
  <?php
- 	echo "<br/>".$display;
+  echo "<br/>".$display;
  ?>
  <br/>
  <br/>
 
  <div id='calendar'></div>
+
+
+
+
 
 </body>
 

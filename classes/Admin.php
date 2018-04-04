cb0e14ab7bb38e0c49df67399fed7ea49bfa61d4
Sandesh Tiwari <sandeshtiwari@live.com>
2018-04-04 18:08:42 -0500

    feat: Add method to give unique collision id

--- classes/Admin.php
+++ classes/Admin.php
@@ -36,6 +36,18 @@
 			}
 			return json_encode($data);
 		}
+        // method to get a unique occupied id which is not already in the table
+		private function getUniqueOccupiedID()
+		{
+			$string = "SELECT MAX(occupied_ID) as occupied_id FROM occupied";
+			$query = mysqli_query($this->con, $string);
+			$row = mysqli_fetch_assoc($query);
+			if(empty($row))
+			{
+				return 1;
+			}
+			return $row['occupied_id']+1;
+		}
         // method to give days
         private function giveDays($day)
 		{

b049e7f34ab8e7638cccc6dbce11c036e0c864f8
Sandesh Tiwari <sandeshtiwari@live.com>
2018-04-04 18:14:55 -0500

    feat: Add a new page to resolve collision

---
+++ resolveCollision.php
@@ -0,0 +1,21 @@
+<?php
+	require "config/config.php";
+	require "classes/Admin.php";
+	if($_SESSION['privilege'] != 'admin' || !isset($_SESSION['email']))
+	{
+  		header("Location: index.php");
+	}
+	if(!empty($_GET))
+	{
+		$admin = new Admin($con, $_SESSION['email']);
+		if(isset($_GET['override']))
+		{
+			$admin->giveAccess($_GET['collidingCourse'], $_GET['course_id'],$_GET['room_id'],$_GET['week_id'],$_GET['day'],$_GET['semester_id']);
+			header("Location: collision.php?accessGranted");
+		}
+	}
+	else
+	{
+		header("Location: index.php");
+	}
+?>

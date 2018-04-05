2eab6a4c83f34741c0e891f2f94f9b4e13d59d86
Sandesh Tiwari <sandeshtiwari@live.com>
2018-04-04 20:25:18 -0500

    feat: Change resolveCollision.php to delete request

--- resolveCollision.php
+++ resolveCollision.php
@@ -13,6 +13,11 @@
 			$admin->giveAccess($_GET['collidingCourse'], $_GET['course_id'],$_GET['room_id'],$_GET['week_id'],$_GET['day'],$_GET['semester_id']);
 			header("Location: collision.php?accessGranted");
 		}
+		else if(isset($_GET['delete']))
+		{
+			$admin->deleteRequest($_GET['course_id'],$_GET['room_id'],$_GET['week_id'],$_GET['day'],$_GET['semester_id']);
+			header("Location: collision.php?deleted");
+		}
 	}
 	else
 	{

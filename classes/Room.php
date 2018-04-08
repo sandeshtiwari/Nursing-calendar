8b978d712ae1373b43dee855abe8c6f20fed1ae9
Sandesh Tiwari <sandeshtiwari@live.com>
2018-04-08 10:47:52 -0500

    feat: Add method to check if a class is booked for a  day

--- classes/Room.php
+++ classes/Room.php
@@ -72,6 +72,18 @@
 			}
 			return $occupiedDays;
 		}
+        // method to check if a room is vacant for a single day for a course
+        public function checkVacancyForSingleDay($day, $week_id, $semester_id, $room_id)
+		{
+			$string = "SELECT Course_ID FROM occupied WHERE Week_ID = $week_id AND $day = 'yes' AND Semester_ID = $semester_id AND Room_ID = $room_id";
+			$query = mysqli_query($this->con, $string);
+			$result = mysqli_fetch_assoc($query);
+			if(!empty($result))
+			{
+				return "request";
+			}
+			return "book";
+		}
 		// method to check if a room is already requested for a course
 		public function checkRequested($course_id, $room_id, $semester_id, $weeks, $requestedDays)
 		{

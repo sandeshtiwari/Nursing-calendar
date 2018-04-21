623adfec2d072eb4d37062a08b0c8c29a6da614c
Sandesh Tiwari <sandeshtiwari@live.com>
2018-04-21 18:32:42 -0500

    feat: Add method to get the latest sem for the Room class

--- classes/Room.php
+++ classes/Room.php
@@ -64,6 +64,13 @@
 				$dow[] = 5;
 			return $dow;
 		}
+        public function getLatestSem()
+		{
+			$string = "SELECT ID, MAX(end_date) FROM semester LIMIT 1";
+			$query = mysqli_query($this->con, $string);
+			$sem = mysqli_fetch_assoc($query);
+			return $sem['ID'];
+		}
 		// method to get all the occupied rooms IDs for a given course
 		public function getOccupiedRoomAndDays($course_id,$semester_id,$weeks)
 		{

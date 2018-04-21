a1ed9b5c6ecda471b27c45e4156c4e65c6b5e317
Aliaksandra <danisheuskaya@gmail.com>
2018-04-21 18:23:21 -0500

    Removed unnesesary calendar code on the Admin_calendar since we emeding it from the external file

--- admin_calendar.php
+++ admin_calendar.php
@@ -33,61 +33,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.min.js"></script>
-<script>
- $(document).ready(function() {
-  $('#nurcalendar').fullCalendar({ 
-        eventRender: function(event, element, view) {
-        var theDate = event.start
-        var endDate = event.dowend;
-        var startDate = event.dowstart;
-        
-        if (theDate >= endDate) {
-                return false;
-        }
 
-        if (theDate <= startDate) {
-          return false;
-        }
-        
-        },
-
-        defaultView: 'month',
-        nowIndicator: true,
-        header: {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'month,agendaWeek,agendaDay'
-        },
-        //defaultDate: '2016-01-15T16:00:00',
-        events: giveEvents()
-  });
-  function giveEvents()
-  {
-    var data = <?php echo $json;?>;
-    console.log(data[0]);
-    for(i = 0; i < data.length; i++)
-    {
-      data[i]['dowstart'] = new Date(data[i]['dowstart']);
-      data[i]['dowend'] = new Date(data[i]['dowend']);
-    }
-    return data;
-  }
-});
-</script>
-<style>
-
- body {
-  margin-top: 40px;
-  text-align: center;
-  font-size: 14px;
-  font-family: "Lucida Grande",Helvetica,Arial,Verdana,sans-serif;
-  }
-
- #nurcalendar {
-  width: 650px;
-  margin: 0 auto;
-  }
-</style>
 
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
